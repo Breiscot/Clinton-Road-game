faeb278b37fbfc7e38c1d567780976c5a7ee5d02
Breiscot <omegaxgaming09@gmail.com>
2025-12-16 09:26:15 +0100

    I added grass texture

--- scripts/forest_managerV2.gd
+++ scripts/forest_managerV2.gd
@@ -2,6 +2,7 @@ extends Node3D
 
 func _ready():
 	setup_environment()
+	setup_grass_texture()
 	print("Forest ready")
 	
 func setup_environment():
@@ -27,3 +28,33 @@ func setup_environment():
 	env.fog_density = 0.02
 	
 	world_env.environment = env
+	
+func setup_grass_texture():
+	var floor_mesh: MeshInstance3D = null
+	
+	if has_node("Floor/MeshInstance3D"):
+		floor_mesh = $Floor/MeshInstance3D
+	elif has_node("Floor"):
+		for child in $Floor.get_children():
+			if child is MeshInstance3D:
+				floor_mesh = child
+				break
+				
+	if floor_mesh == null:
+		return
+		
+	# Crea materiale erba
+	var grass_mat := StandardMaterial3D.new()
+	
+	var grass_texture = load("res://images/Grass003_1K-JPG_Color.jpg")
+	if grass_texture:
+		grass_mat.albedo_texture = grass_texture
+	else:
+		grass_mat.albedo_color = Color(0.08, 0.15, 0.05)
+	
+	# Ripeti texture	
+	grass_mat.uv1_scale = Vector3(20, 20 ,20)
+	grass_mat.roughness = 0.9
+	
+	floor_mesh.material_override = grass_mat
+	
