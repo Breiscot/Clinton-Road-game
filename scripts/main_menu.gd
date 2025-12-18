d8a6bb862a94b71524e09222d6943a85826306d3
Breiscot <omegaxgaming09@gmail.com>
2025-12-18 14:48:10 +0100

    progress with options_menu in main_menu

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,8 +1,21 @@
 extends Control
-
+# Menu
 @onready var play_button := $VBoxContainer/PlayButton
 @onready var options_button := $VBoxContainer/OptionsButton
 @onready var exit_button := $VBoxContainer/ExitButton
+@onready var main_container := $VBoxContainer
+
+# Options
+@onready var options_menu := $OptionsMenu
+@onready var sensitivity_slider := $OptionsMenu/VBoxContainer/SensitivityContainer/SensitivitySlider
+@onready var volume_slider := $OptionsMenu/VBoxContainer/VolumeContainer/VolumeSlider
+@onready var fullscreen_check := $OptionsMenu/VBoxContainer/FullscreenContainer/FullscreenCheck
+@onready var back_button := $OptionsMenu/VBoxContainer/BackButton
+
+# Audio
+@onready var music_player := $AudioManager/MusicPlayer
+@onready var ambience_player := $AudioManager/AmbientPlayer
+@onready var hover_sound := $AudioManager/HoverSound
 
 # Scena
 @export var game_scene: PackedScene
@@ -11,20 +24,60 @@ func _ready():
 	# Mostra il mouse
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	
+	# Nascondi l'options menu all'inizio
+	if options_menu:
+		options_menu.visible = false
+	
 	# Connetti i bottoni
 	play_button.pressed.connect(_on_play_pressed)
 	options_button.pressed.connect(_on_options_pressed)
 	exit_button.pressed.connect(_on_exit_pressed)
 	
+	# Connetti HoverSound ai Button
+	setup_button_sounds()
+	
+	# Connetti Controlli Options
+	if back_button:
+		back_button.pressed.connect(_on_back_pressed)
+	if sensitivity_slider:
+		sensitivity_slider.value_changed.connect(_on_sensitivity_changed)
+	if volume_slider:
+		volume_slider.value_changed.connect(_on_volume_changed)
+	if fullscreen_check:
+		fullscreen_check.toggled.connect(_on_fullscreen_toggled)
+	
+	# Carica Options
+	load_settings()
+	
 	# Focus sul primo bottone
 	play_button.grab_focus()
+	
+	# Style nei Button
+	style_buttons()
+	
+	# Avvia Audio
+	start_audio()
+	
 	# Animazione Fade in
 	modulate.a = 0
 	var tween = create_tween()
 	tween.tween_property(self, "modulate:a", 1.0, 0.5)
 	
-	# Style nei Button
-	style_buttons()
+# Audio
+func start_audio():
+	# Avvia musica in Fade In
+	if music_player and music_player.stream:
+		music_player.volume_db = -40
+		music_player.play()
+		var tween = create_tween()
+		tween.tween_property(music_player, "volume_db", -10.0, 2.0)
+	
+	# Avvia ambiente
+	if ambience_player and ambience_player.stream:
+		ambience_player.volume_db = -40
+		ambience_player.play()
+		var tween = create_tween()
+		tween.tween_property(ambience_player, "volume_db", -15.0, 2.0)
 	
 func style_buttons():
 	var buttons = [play_button, options_button, exit_button]
