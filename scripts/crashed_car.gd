3e5975fb5adb283090eb19183fd6149010819720
Breiscot <omegaxgaming09@gmail.com>
2025-12-22 15:20:29 +0100

    added more modifieds at crashed_car.gd

--- scripts/crashed_car.gd
+++ scripts/crashed_car.gd
@@ -57,7 +57,96 @@ func setup_car_damage():
 	position.x += 2.5
 	
 func setup_smoke_particles():
+	smoke_particles = GPUParticles3D.new()
+	smoke_particles.name = "SmokeParticles"
+	smoke_particles.amount = 50
+	smoke_particles.lifetime = 3.0
+	smoke_particles.emitting = true
+	smoke_particles.position = Vector3(0, 1.2, 0.8)
 	
+	var smoke_mat := ParticleProcessMaterial.new()
+	smoke_mat.emission_shape = ParticleProcessMaterial.EMISSION_SHAPE_BOX
+	smoke_mat.emission_box_extents = Vector3(0.3, 0.1, 0.3)
+	smoke_mat.direction = Vector3(0, 1, 0)
+	smoke_mat.spread = 15.0
+	smoke_mat.initial_velocity_min = 0.5
+	smoke_mat.initial_velocity_max = 1.5
+	smoke_mat.gravity = Vector3(0, -0.2, 0)
+	smoke_mat.scale_min = 0.5
+	smoke_mat.scale_max = 2.0
+	
+	var gradient := Gradient.new()
+	gradient.add_point(0.0, Color(0.4, 0.4, 0.4, 0.6))
+	gradient.add_point(1.0, Color(0.2, 0.2, 0.2, 0.0))
+	var gradient_tex := GradientTexture1D.new()
+	gradient_tex.gradient = gradient
+	smoke_mat.color_ramp = gradient_tex
+	
+	smoke_particles.process_material = smoke_mat
+	
+	var quad := QuadMesh.new()
+	quad.size = Vector2(1, 1)
+	smoke_particles.draw_pass_1 = quad
+	
+	var visual_mat := StandardMaterial3D.new()
+	visual_mat.transparency = BaseMaterial3D.TRANSPARENCY_ALPHA
+	visual_mat.shading_mode = BaseMaterial3D.SHADING_MODE_UNSHADED
+	visual_mat.billboard_mode = BaseMaterial3D.BILLBOARD_PARTICLES
+	visual_mat.vertex_color_use_as_albedo = true
+	visual_mat.albedo_color = Color(0.5, 0.5, 0.5, 1.0)
+	smoke_particles.material_override = visual_mat
+	
+	add_child(smoke_particles)
+	
+func setup_interaction_area():
+	if has_node("InteractionArea"):
+		interaction_area = $InteractionArea
+	else:
+		interaction_area = Area3D.new()
+		interaction_area.name = "InteractionArea"
+		
+		var collision = CollisionShape3D.new()
+		var shape = BoxShape3D.new()
+		shape.size = Vector3(3, 2, 3)
+		collision.shape = shape
+		collision.position = Vector3(0, 1, 2.5)
+		
+		interaction_area.add_child(collision)
+		add_child(interaction_area)
+		
+	# Connetti segnali
+	interaction_area.body_entered.connect(_on_interaction_area_entered)
+	interaction_area.body_exited.connect(_on_interaction_area_exited)
+	
+	print("Interaction area setup.")
+	
+func setup_interaction_prompt():
+	interaction_prompt = Node3D.new()
+	interaction_prompt.name = "InteractionPrompt"
+	interaction_prompt.position = Vector3(0, 2.5, 2.5)
+	
+	prompt_label = Label3D.new()
+	prompt_label.text = get_prompt_text()
+	prompt_label.font_size = 32
+	prompt_label.modulate = Color(1, 1, 1, 1)
+	prompt_label.billboard = BaseMaterial3D.BILLBOARD_ENABLED
+	prompt_label.no_depth_test = true
+	prompt_label.outline_size = 8
+	prompt_label.outline_modulate = Color(0, 0, 0, 1)
+	
+	interaction_prompt.add_child(prompt_label)
+	add_child(interaction_prompt)
+	
+	# Nascondi all'inizio
+	interaction_prompt.visible = false
+		
+func get_prompt_text():
+	if car_repaired:
+		return "[E] Escape!"
+	elif collected_parts >= required_parts:
+		return "[E] Repair Car"
+	else:
+		return"[E] Repair Car\n" + str(collected_parts) + "/" + str(required_parts) + " parts"
 
 func start_effects():
 	# Fumo che esce dal motore
@@ -67,6 +156,7 @@ func start_effects():
 func _process(delta):
 	update_hazard_lights(delta)
 	update_headlight_flicker(delta)
+	update_prompt()
 
 func update_hazard_lights(delta):
 	hazard_timer += delta
@@ -91,3 +181,6 @@ func update_headlight_flicker(delta):
 			headlight_left.light_energy = randf_range(0.3, 1.0)
 		else:
 			headlight_left.light_energy = 0.0
+			
+func update_prompt():
+	
